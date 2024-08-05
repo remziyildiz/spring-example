66dc1d48e05b9544c0103c3878613c266df1ea75
remziyildiz <133529507+remziyildiz@users.noreply.github.com>
2024-08-05 19:12:33 +0300

    Add files via upload

---
+++ spring-example/spring-datajpa-postgresql/src/main/java/com/haydikodlayalim/dto/KisiDto.java
@@ -0,0 +1,18 @@
+package com.haydikodlayalim.dto;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class KisiDto {
+
+    private Long id;
+
+    private String adi;
+
+    private String soyadi;
+
+    private List<String> adresler;
+
+}
+
---
+++ spring-example/spring-elasticsearch/src/main/java/com/haydikodlayalim/api/KisiController.java
@@ -0,0 +1,45 @@
+package com.haydikodlayalim.api;
+
+
+import com.haydikodlayalim.entity.Kisi;
+
+import java.util.Calendar;
+import java.util.List;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.KisiRepository;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/kisi")
+public class KisiController{
+
+    private final KisiRepository kisiRepository;
+
+    @PostConstruct
+    public void init(){
+        Kisi kisi = new Kisi();
+        kisi.setAd("Haydi");
+        kisi.setSoyad("Kodlayalim");
+        kisi.setAdres("test");
+        kisi.setDogumTarihi(Calendar.getInstance().getTime());
+        kisi.setId("K0001");
+        kisiRepository.save(kisi);
+    }
+
+    @GetMapping("/{search}")
+    public ResponseEntity<List<Kisi>>getKisi(@PathVariable String search){
+        List<Kisi> kisiler = kisiRepository.findByAdLikeOrSoyadLike(search, search);
+        return ResponseEntity.ok(kisiler);
+
+    }
+}
+
+
+
